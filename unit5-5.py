95e5f1366d35a0cb9aa315070ed6b4be9b509478
Hello_world <lu>
2018-01-18 20:32:41 -0500

    Finished

--- unit5-5.py
+++ unit5-5.py
@@ -1,30 +1,44 @@
-# Created by: luca.ienzi
-# Created on: dec 2017
+# Created by: Luca
+# Created on: Dec 2017
 # Created for: ICS3U
-# this program will 2d arreas 
-# Create a list.
-elements = []
+# This finds the average of a 2D array with random numbers
 
-# Append empty lists in first two indexes.
-elements.append([])
-elements.append([])
+from numpy import random
 
-# Add elements to empty lists.
-elements[0].append(1)
-elements[0].append(2)
-
-elements[1].append(3)
-elements[1].append(4)
-
-# Display top-left element.
-print(elements[0][0])
-
-# Display entire list.
-print(elements)
-
-# Loop over rows.
-for row in elements:
-    # Loop over columns.
-    for column in row:
-        print(column)
+def two_dimension_loop_average(array):
+    # finds average in passed 2D array
+    total = 0
     
+    for row in array:
+        for column in row:
+            total = total + column
+    average = total / (len(array[0]) * len(array))
+    
+    return average
+
+
+row_number = int(raw_input("Enter the number of rows: "))
+if row_number < 0:
+    row_number = int(raw_input("Please enter a positive number for the number of rows: "))
+else:
+    pass
+
+column_number = int(raw_input("Enter  number of columns: "))
+if column_number < 0:
+    column_number = int(raw_input("Please enter a positive number for the number of columns: "))
+else:
+    pass
+
+
+table = []
+
+for row in range(0, row_number):
+    table.append([])
+    for column in range(0, column_number):
+        table[row].append(random.randint(0, 50+1))
+
+average_of_element_table = two_dimension_loop_average(table)
+
+
+print(table)
+print("\nThe average of your numbers in the table is approximately: " + str(average_of_element_table) + ".\n")
